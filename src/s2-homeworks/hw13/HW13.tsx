e48a78e5a0bcafa662f2b32da5f1c70e5b12c7cf
Seidali001 <seitmambetovseidali@gmail.com>
2023-03-05 16:54:32 +0000

    Described the types of hooks, props, arrays and functions. And added the logic of some functions. And displayed some incoming data.

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -37,7 +37,7 @@ const HW13 = () => {
                 setCode('Код 200!')
                 setImage(success200)
                 // дописать
-                setText('...всё ок)')
+                setText(res.data.errorText)
                 setInfo(res.data.Info)
 
             })
